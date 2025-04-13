e10f0959c4190c75785d01210d4755f0cf27178a
nikayel <alinikayeljamal@gmail.com>
2025-04-12 23:01:17 -0700

    Define Home,singUp and SignIn routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './components/Auth/Login';
+import './components/Auth/SignUp';
+import './components/HomePage';
+import { Route } from 'lucide-react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          REACT
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+        </Routes>
+    </Router>
+  )
 }
 
 export default App;
