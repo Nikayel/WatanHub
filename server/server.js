a6c6060dbb0d50af168b605fa2006c18bb4b690c
nikayel <alinikayeljamal@gmail.com>
2025-04-28 09:17:03 -0700

    make sure app listens on both PORT and onrender CUSTOM . adress

--- server/server.js
+++ server/server.js
@@ -67,6 +67,6 @@ app.post('/api/contact', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',() => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
